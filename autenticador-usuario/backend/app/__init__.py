207e44f0e903f9ac8c71cf72d8f88ed92035a3a8
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-08-26 18:02:41 -0300

    Estructura proyecto autenticador-usuario

---
+++ autenticador-usuario/backend/app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+import jwt
+import datetime
+
+app = Flask(__name__)
+
+
+#Inicio de dependencias
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
+#Modelos
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
